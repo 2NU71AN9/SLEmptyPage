bfb0db41e5a7cf0f0606564c2ed79aabb74e59ce
孙梁 <1145373054@qq.com>
2018-08-14 17:37:00 +0800

    update

--- SLEmptyPage/AppDelegate.swift
+++ SLEmptyPage/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         SLEmptyPageManager.enable = true
-        SLEmptyView.defaultText = "11111"
         return true
     }
 
